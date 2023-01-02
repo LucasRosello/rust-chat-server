072a9bbd4d132ab926eb6a3ef550be435bce03ec
Lucas Rosello <l.martin.rosello@gmail.com>
2023-01-02 20:29:07 +0000

    making the bases

--- main.rs
+++ main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("First Commit")
+#[tokio::main]
+async fn main() {
+
+    let listener: TcpListener = TcpListener::bind("localhost:8080").await;
+
+    let (mut socket: TcpListener, addr: SocketAddr) = listener.accept().await.unwrap();
+
+    let mut buffer: [u8; _] = [0u8; 1024];
+
+    let bytes_read: usize = socket.read(&mut buffer).await.unwrap();
+
+    socket.write_all()
 }
